cd51174701cffb9d9df467432ed427e53baca331
Udin Rajkarnikar <udin_rajkarnikar2001@yahoo.com>
2024-01-15 22:51:17 -0600

    Some styling added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+import './categories.styles.scss';
+
 const App = () => {
+
+  const categories = [
+
+    {
+      id: 1,
+      category:'Hats',
+    },
+
+    {
+      id: 2,
+      category:'Jackets',
+    },
+
+    {
+      id: 3,
+      category:'Sneakers',
+    },
+
+    {
+      id: 4,
+      category:'Men',
+    },
+
+    {
+      id: 5,
+      category:'Women',
+    },
+
+    
+
+  ]
+
   return (
-  <div>
-    Hello World!!
+  <div className="categories-container">
+    {categories.map((item) => (
+        <div className="category-container" key={item.id}>
+          <div className="category-container-body">
+            <h1>{item.category}</h1>
+            <p>Shop Now</p>
+          </div>
+        </div>
+    ))}
   </div>
   );
 };
