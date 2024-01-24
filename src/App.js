aeff156f074097e8e1c9a69d1949aae55de33413
Udin Rajkarnikar <udin_rajkarnikar2001@yahoo.com>
2024-01-23 20:37:25 -0600

    some CSS fixes

--- src/App.js
+++ src/App.js
@@ -3,42 +3,41 @@ import './categories.styles.scss';
 const App = () => {
 
   const categories = [
-
     {
-      id: 1,
-      category:'Hats',
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
-
     {
-      id: 2,
-      category:'Jackets',
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
-
     {
-      id: 3,
-      category:'Sneakers',
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
-
     {
-      id: 4,
-      category:'Men',
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
     },
-
     {
-      id: 5,
-      category:'Women',
-    },
-
-    
-
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
   ]
+  
 
   return (
   <div className="categories-container">
     {categories.map((item) => (
         <div className="category-container" key={item.id}>
+          <div className='background-image' style={{backgroundImage: `url(${item.imageUrl})`}}/>
           <div className="category-container-body">
-            <h1>{item.category}</h1>
+            <h1>{item.title}</h1>
             <p>Shop Now</p>
           </div>
         </div>
